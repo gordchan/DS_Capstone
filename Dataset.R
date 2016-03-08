65a79891a2d1bbab8bdf42fb2b65d0d4987525be
gordchan <gordon.chan@mensa.org.hk>
2016-03-08 04:10:06 +0000

    Source path added.

---
+++ Dataset.R
@@ -0,0 +1,15 @@
+###
+### Loading the datasets
+###
+
+
+# Filepaths -------------------------------------
+
+## Blogs
+path.blogs <- file.path("dataset", "en_US", "en_US.blogs.txt")
+
+# News
+path.news <- file.path("dataset", "en_US", "en_US.news.txt")
+
+# Twitter
+path.twitter <- file.path("dataset", "en_US", "en_US.twitter.txt")
